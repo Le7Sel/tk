6914cc5ee4609cba54217e9e900d7e4b4c60c9a0
Le7Sel <nathanbillard12@gmail.com>
2021-05-25 09:06:37 +0200

    better (working) matrices dimension check

--- multiply_matrices.py
+++ multiply_matrices.py
@@ -2,20 +2,13 @@
 # multiply 2 matrices together
 from tkinter import *
 
-root = Tk()
-root.title("Multiply Matrices")
-pad=10
-
 def multMatrix(m1, m2):
-    if len(m1[0]) != len(m2):
-        throw(typeError, 'The matrices don\' have the same size')
-    else:
-        result = [[0 for x in range(len(m2[0]))] for y in range(len(m1))]
-        for x in range(len(m2[0])):
-            for y in range(len(m1)):
-                for i in range(len(m2)):
-                    result[y][x] += m1[y][i] * m2[i][x]
-        return result
+    result = [[0 for x in range(len(m2[0]))] for y in range(len(m1))]
+    for x in range(len(m2[0])):
+        for y in range(len(m1)):
+            for i in range(len(m2)):
+                result[y][x] += m1[y][i] * m2[i][x]
+    return result
 
 def makeMatrix(parent, height, width, padding, content='empty', packDir='Left' ):
     mat = Frame(parent)
@@ -53,16 +46,25 @@ def writeMat(dest, mat):
 def equal():
     writeMat(resultMat, multMatrix(readMat(mat1), readMat(mat2)))
 
-mat1X = int(input('How many columns does the first matrice have? : ')
-mat1Y = int(input('How many rows does the first matrice have? : ')
-mat2X = int(input('How many columns does the second matrice have? : ')
-mat2Y = int(input('How many rows does the second matrice have? : ')
 
-mat1 = makeMatrix(root, mat1Y, mat1X, pad)
-Label(root, text="x").pack(side=LEFT)
-mat2 = makeMatrix(root, mat2Y, mat2X, pad)
-Button(root, text='=', command=equal).pack(side=LEFT)
-resultMat = makeMatrix(root, mat1Y, mat2X, pad, 'result')
+mat1Y = int(input('How many rows does the first matrice have? : '))
+mat1X = int(input('How many columns does the first matrice have? : '))
+mat2Y = int(input('How many rows does the second matrice have? : '))
+mat2X = int(input('How many columns does the second matrice have? : '))
+
+if (mat1X == mat2Y):
+    root = Tk()
+    root.title("Multiply Matrices")
+
+    pad=10
+
+    mat1 = makeMatrix(root, mat1Y, mat1X, pad)
+    Label(root, text="x").pack(side=LEFT)
+    mat2 = makeMatrix(root, mat2Y, mat2X, pad)
+    Button(root, text='=', command=equal).pack(side=LEFT)
+    resultMat = makeMatrix(root, mat1Y, mat2X, pad, 'result')
+    root.mainloop()
+else:
+    print("These matrices cannot be multiplied")
 
 
-root.mainloop()
